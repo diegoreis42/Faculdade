785b5085b2be01b459f2b3170163e523d23b9874
merlin <reisdiego144@gmail.com>
2023-03-24 11:01:37 -0300

    Criando funçao 'saveMatrixFile' e lendo entrada inline'

--- grafos/at1.py
+++ grafos/at1.py
@@ -1,4 +1,19 @@
 import numpy as np
+import sys
+
+
+path = '/home/merlin/study/Faculdade/grafos/Instancias/' 
+
+
+if __name__ == "__main__":
+    """
+     Arguments of the script
+
+     Parameters
+     ------------------
+     [1]InstanceName, [2]InstanceID
+    """
+    InstanceName, InstanceID = str(sys.argv[1]), str(sys.argv[2])
 
 
 def createDefaultMatrix(inst, id):
@@ -11,14 +26,33 @@ def createDefaultMatrix(inst, id):
     inst (Name's Instance)
     id   (Id's Instance)
     """
-    path = '/home/merlin/study/Faculdade/grafos/Instancias/' + inst + id + '.txt'
+    pathFile = path + inst + id + '.txt'
 
-    with open(path, 'rb') as f:
+    with open(pathFile, 'rb') as f:
         nrows, ncols = [int(field) for field in f.readline().split()]
         data = np.genfromtxt(f, dtype='int32', max_rows=nrows)
 
     return data
 
 
-data = createDefaultMatrix('teste', '1')
-print(data)
+
+def saveMatrixFile(InstanceName, data):
+    """
+    Print the name and size of the matrix, and save it in a file
+
+    Parameters
+    ------------------
+    InstanceName (string)
+    data (ndarray)
+    """
+    print('Salvando {} de formato {}' .format(InstanceName, data.shape))
+    
+    file = open('ans.txt', 'w')
+    file.write(InstanceName + ' ' + str(data.shape[0]) + ' ' + str(data.shape[1]) + '\n')
+
+
+# Executando codigo
+
+data = createDefaultMatrix(InstanceName, InstanceID)
+
+saveMatrixFile(InstanceName, data)
