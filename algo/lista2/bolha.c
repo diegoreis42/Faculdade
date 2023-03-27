99ac0f792b0bc59851c1e231091c77d50c1ef03d
merlin <reisdiego144@gmail.com>
2023-03-27 09:31:29 -0300

    adicionando comentarios

--- algo/lista2/bolha.c
+++ algo/lista2/bolha.c
@@ -1,7 +1,14 @@
+/* Made by Diego Reis 27/03/2023
+---------------------------------
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
-
+// Armazena:
+// Vetor de inteiros
+// Tamanho do vetor
+// Valor do erro: 1, 2 ou 3; especificados na funçao trataErro
 typedef struct tReturn {
   int *vet;
   int tam;
@@ -27,6 +34,7 @@ int getErro(tReturn *rep){
   return rep->erro;
 }
 
+// recebe tReturn rep->erro;
 void trataErro(int erro){
   if(erro == 1)
     printf("Erro ao abrir o arquivo\n");
@@ -36,15 +44,18 @@ void trataErro(int erro){
     printf("Erro ao alocar memória\n");
 }
 
+// Le arquivo contendo sequencia de inteiros
+// Primeiro valor eh o tamanho da sequencia
+// Retorna: tReturn contendo o vetor, tamanho, e tipo de erro(se houver)
 tReturn* leArquivo(char nomeArquivo[]){
   tReturn *rep = alocaReturn();
   FILE *fptr = fopen(nomeArquivo, "r");
-
+// Checa erro ao abrir o arquivo
   if(fptr == NULL){
     rep->erro = 1;
     return rep;
   }
-
+// Checa erro ao alocar memoria
   if(rep == NULL){
     rep->erro = 3;
     return rep;
@@ -55,6 +66,7 @@ tReturn* leArquivo(char nomeArquivo[]){
   rep->vet = (int*) malloc(sizeof(int) * (rep->tam));
   
   for(int i = 0; i < (rep->tam); i++){
+    // Checa erro ao ler o arquivo
     if(fscanf(fptr, "%d", &rep->vet[i]) != 1){
         rep->erro = 2;
         return rep;
@@ -63,7 +75,11 @@ tReturn* leArquivo(char nomeArquivo[]){
   }
   return rep;
 }
-
+/*  Algoritmo de ordenaçao Bubble sort
+ * -------------------------------------
+ *  Pior caso -> O(n^2)
+ *  Melhor caso -> O(n^2)
+ * */
 
 int bolha(int *vet, int tam){
   int aux, counter = 0;
@@ -81,6 +97,17 @@ int bolha(int *vet, int tam){
     return counter;
 }
 
+/*  Algoritmo de ordenaçao Smart Bubble sort
+ * ------------------------------------------
+ *  Pior caso -> O(n^2)
+ *  Melhor caso -> O(n)
+ *  Explicaçao
+ * ------------------------------------------
+ *  Como maior numero da parte desordenada sempre
+ *  vai ser posto na posiçao correta, eh desneces-
+ *  sario checar os valores apos ele, por isto o
+ *  tam - i.
+ * */
 int bolhaInteligente(int *vet, int tam){
 
   int aux, counter = 0;
